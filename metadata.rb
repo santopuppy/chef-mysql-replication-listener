365b09c95770f209ad865c44d3aa8b6dbe767879
Lester Celestial <lesterc@sourcepad.com>
2014-02-10 16:26:26 +0800

    updated the metadata license

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'mysql-replication-listener'
 maintainer       'Lester Celestial'
 maintainer_email 'lesterc@sourcepad.com'
-license          'All rights reserved'
+license          'Apache 2.0'
 description      'Installs mysql-replication-listener'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
